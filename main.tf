7d0bb4006ae0cf682ee63d2ad317d0e093c86abb
Yannick  Kalukuta <yannickkalukuta@gmail.com>
2025-08-19 20:35:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,8 +72,7 @@ resource "aws_instance" "app" {
   instance_type = var.instance_type
   subnet_id = aws_subnet.public.id
   security_groups = [aws_security_group.ec2_sg.id]
-  key_name = var.key_pair_name
-
+  
   tags = {
     Name = "${var.project_name}-ec2"
   }
